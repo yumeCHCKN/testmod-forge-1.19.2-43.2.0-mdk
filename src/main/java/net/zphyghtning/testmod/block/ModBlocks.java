f064cd4c76b51d8aba40f7bbdcc15da9f6c30e98
yumeCHCKN <zaidaan2017@gmail.com>
2023-02-25 16:57:45 +0700

    Added "Vase"

--- src/main/java/net/zphyghtning/testmod/block/ModBlocks.java
+++ src/main/java/net/zphyghtning/testmod/block/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.zphyghtning.testmod.block.custom.Vase;
 import net.zphyghtning.testmod.item.ModCreativeModeTab;
 import net.zphyghtning.testmod.item.ModItems;
 
@@ -29,6 +30,10 @@ public class ModBlocks {
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)), ModCreativeModeTab.TEST_TAB);
+
+    public static final RegistryObject<Block> VASE = registerBlock("vase",
+            () -> new Vase(BlockBehaviour.Properties.of(Material.CLAY)
+                    .strength(0.6f).requiresCorrectToolForDrops().noOcclusion()), ModCreativeModeTab.TEST_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
